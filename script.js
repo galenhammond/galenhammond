5b809cb0169067877723223ffadb682e804f0005
galenhammond <34431711+galenhammond@users.noreply.github.com>
2017-12-10 23:21:17 -0500

    School Project

---
+++ script.js
@@ -0,0 +1,15 @@
+<script>
+$("#imgCont").mouseover(function() {
+    $('#imgCont').fadeOut("slow", 0);
+}) $('imgCont').mouseout(function() {
+	#('imgCont').fadeOut('slow' 1);
+});
+</script>
+
+<script>
+$(function(){
+    $(".flip").flip({
+        trigger: 'hover'
+    });
+});
+</script>
